1240732c104bf10b136db10595af118924bbc173
alexgoudemond <72508149+alexgoudemond@users.noreply.github.com>
2022-04-24 17:24:06 +0200

    Created bank_note_recognition.py

---
+++ bank_note_recognition.py
@@ -0,0 +1,16 @@
+'''
+Authors:    Sumeith Ishwanthlal (219006284)
+            Alexander Goudemond (219030365)
+
+Program name: bank_note_recognition.py
+
+Goal:
+
+Summary:
+
+'''
+
+print("\n---Program Starting---")\
+
+
+print("\n---Program Ending---")
